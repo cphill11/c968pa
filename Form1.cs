affe9177ba842d5faec3fdde55239db4dd34b528
cphill11 <criste.phillips@yahoo.com>
2026-04-03 17:18:54 -0400

    2nd form added, issues with design on form 1 debugged

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,7 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+
 
 namespace c968pa
 {
@@ -31,5 +32,9 @@ namespace c968pa
         {
 
         }
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    label2.Text = "Add Item";
+        //}
     }
 }
