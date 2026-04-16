e8c475407348cce1721817f8c97a81dfd1fe6006
cphill11 <criste.phillips@yahoo.com>
2026-04-16 14:46:23 -0400

    Initial push for inventory, product, and part classes.

---
+++ Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c968pa
+{
+    public class Inventory
+    {
+        public BindingList<Product> Products { get; set; } = new BindingList<Product>();
+        public BindingList<Part> AllParts { get; set; } = new BindingList<Part>();
+    }
+}
